5e137cd216fc9131f9b70c314fbf339bbf79bf64
joseSantorcuato <santorcuato76@gmail.com>
2018-03-27 22:46:41 -0300

    Se agrega conexión python firebase automatizado e ingreso data

--- pyFirebase/ingresofbasesecond.py
+++ pyFirebase/ingresofbasesecond.py
@@ -14,7 +14,7 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
-#authenticate a user
+
 user = auth.sign_in_with_email_and_password("tusuarios@sumail.com", "lapasswordsecreta")
 db = firebase.database()
 usuario=str(user['idToken'])
@@ -29,5 +29,5 @@ while True:
     time.sleep( 1 )
     if controlb == 59:
         print ("llegue a 59")
-        tutabla = {"campo1": "ok", "campo2": "ok", "campo3": "ok", "campo44": "ok"}
+        tutabla = {"campo1": "ok", "campo2": "ok", "campo3": "ok", "campo4": "ok"}
         db.child("campos").push(tutabla, user['idToken'])
