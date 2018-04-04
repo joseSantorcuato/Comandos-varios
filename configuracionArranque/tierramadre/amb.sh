ef0c65453d4f4359e025195f8f88ed2f6e3e729e
joseSantorcuato <santorcuato76@gmail.com>
2018-04-04 10:18:30 -0300

    Se agrega activación bin/bash virtualenv y ejecución automática script python3

---
+++ configuracionArranque/tierramadre/amb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source "/root/Escritorio/tierramadre/bin/activate"
+
+python3 "/root/Escritorio/prueba.py"
---
+++ configuracionArranque/tierramadre/prueba.py
@@ -0,0 +1,13 @@
+import time
+import pyrebase
+from time import gmtime, strftime
+
+
+while True:
+    control = strftime("%S", gmtime())
+    print (control)
+    controlb = int(control)
+
+    time.sleep( 1 )
+    if controlb == 59:
+        print ("llegue a 59")
